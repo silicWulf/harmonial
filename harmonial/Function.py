d76003c1464e0f1887a4c5f0bdca2936c0db2981
silicWulf <me@siliconwolf.pw>
2017-06-14 16:33:49 -0400

    Fixed git error; see previous commit

--- harmonial/Function.py
+++ harmonial/Function.py
@@ -1,20 +1,34 @@
+import pickle, random
 class Function:
     def __init__(self, function_type, params=None):
         self.function_type = function_type
         self.params = params
         self._function_code = ''
-
-        #if self.params == '$argument':
-
+        self._bytestream = b''
+        self._bs_name = ''
 
         if self.function_type == 'say':
-            self.params = '"' + self.params + '"'
-            self._function_code = """    await bot.send_message(ctx.message.channel, """ + self.params + """)
+            if type(self.params) != str:
+                raise ValueError("'params' must be a string when using function type 'say'")
+            else:
+                self._function_code = """    await bot.send_message(ctx.message.channel, '''""" + self.params + """''')
 """
         elif self.function_type == 'writeFile':
-            p = self.params.split('|||')
-            if len(p) < 2:
-                raise Exception("not enough arguments in 'params'")
+            p = params
+            if type(p) == tuple:
+                if len(p) < 2:
+                    raise Exception("not enough arguments in 'params'")
+                else:
+                    self._function_code = """    open('''""" + p[0] + """''').write('''""" + p[1] + """''')
+"""
             else:
-                self._function_code = """    open('""" + p[0] + """').write('""" + p[1] + """')
+                raise ValueError("'params' must be a tuple when using function type 'writeFile'")
+
+        elif self.function_type == 'uploadFile':
+            self._function_code = """    await bot.send_file(ctx.message.channel, '''""" + self.params + """''')
+"""
+        elif self.function_type == 'define':
+            self._bs_name = params[0] + '-' + str(random.randint(100,999))
+            self._bytestream = pickle.dumps(params[1])
+            self._function_code = """    _USABLE_VARS['""" + str(params[0]) + """'] = pickle.load(open('''$filename-assets/$filename-""" + self._bs_name + """.asset''', 'rb'))
 """
--- harmonial/__init__.py
+++ harmonial/__init__.py
@@ -1,18 +1,29 @@
-import random
+import os, random, pickle, shutil
 from .Function import Function
 from .Command import Command
 #from .LoginInfo import LoginInfo
 
 def build(filename, prefix, commands):
+    if filename.endswith('.py') == False: filename += '.py'
     open(filename, 'w').close()
     f = open(filename, 'a')
-    sss = """import discord, aiohttp, asyncio; from discord.ext.commands import Bot
+    sss = """import os, sys, random, pickle
+import discord, aiohttp, asyncio; from discord.ext.commands import Bot
 bot = Bot('""" + prefix + """', pm_help=False)
+_USABLE_VARS = {}
 
 """
     f.write(sss)
+    onepass = False
     for command in commands:
-        f.write(command._command_code)
+        for func in command.functions:
+            if func._bs_name != '':
+                if onepass == False:
+                    onepass = True
+                    shutil.rmtree(filename + '-assets/', ignore_errors=True)
+                    os.makedirs(filename + '-assets/', exist_ok=True)
+                open(filename + '-assets/' + filename + '-' + str(func._bs_name) + '.asset', 'wb').write(func._bytestream)
+        f.write((command._command_code).replace('$filename', filename))
     f.write("bot.run('TOKEN HERE')")
     f.close()
 
@@ -20,7 +31,7 @@ def special(special_type, params=None):
     if type(special_type) == str:
         if special_type == 'randomNumber':
             if type(params) == tuple:
-                return random.choice(range(params[0], params[1]))
+                return "''' + str(random.choice(range(" + str(params[0]) + ',' + str(params[1]) + "))) + '''"
             else:
                 raise ValueError("'params' must be a tuple when using the 'randomNumber' function")
 
@@ -32,5 +43,16 @@ def special(special_type, params=None):
                     return open(params, 'r').read()
                 except FileNotFoundError:
                     raise FileNotFoundError("file not found at '" + params + "'")
+        elif special_type == 'variableString':
+            if type(params) != str:
+                raise ValueError("'params' must be a string when using the 'variableString' function")
+            else:
+                return """''' + str(_USABLE_VARS['""" + str(params) + """']) + '''"""
+
+        elif special_type == 'rawVariable':
+            if type(params) != str:
+                raise ValueError("'params' must be a string when using the 'rawVariable' function")
+            else:
+                return """_USABLE_VARS['""" + str(params) + """']"""
 
     else: return ''
