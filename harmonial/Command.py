aba7f45ef78e431311cb23fe1fdfca7498091dcd
silicWulf <me@siliconwolf.pw>
2017-11-04 21:09:38 -0400

    2 new command arguments
    $messageAuthorName - string representation of the message's author's name
    $messageTimestamp - string representation of the message's timestamp

--- harmonial/Command.py
+++ harmonial/Command.py
@@ -10,6 +10,9 @@ async def """ + self.commandname + """(ctx):
             function_type = function.function_type
             _function_code = function._function_code.replace('$argument', 'ctx.message.content[' + str(len(self.commandname)) + ' + 1]')
             _function_code = function._function_code.replace('$messageContent', "''' + ctx.message.content + '''")
+            _function_code = function._function_code.replace('$messageAuthorName', "''' + str(ctx.message.author.name) + '''")
+            _function_code = function._function_code.replace('$messageTimestamp', "''' + str(ctx.message.timestamp) + '''")
+            # _function_code = function._function.code.replace('$message')
 
             self._command_code += _function_code
 
